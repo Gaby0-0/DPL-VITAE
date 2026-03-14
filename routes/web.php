58c864d8b8f45edfb087f8647acd4364a1e89bec
Mishell Castillo <147366738+mishellCastillorb@users.noreply.github.com>
2026-03-14 15:26:59 -0600

    CRUD vistas iniciales
    Se agregaron vistas para pacientes, ambulancias, insumos, padecimientos y servicios. Las rutas GET ya estan en la carpeta routes/web.php. Pendiente implementar rutas POST, PUT y DELETE en sus respectivos controladores

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonalController;
+use App\Http\Controllers\InsumosController;
+use App\Http\Controllers\AmbulanciasController;
+use App\Http\Controllers\PadecimientosController;
 
-//ruta que ira por default al abrir la pagina
+//Ruta que irá por default cuando un cliente abra la pagina
 Route::get('/', function () {
     return view('inicio');
 });
 
-//FORM COtIZACION
+//FORM COTIZACION
 Route::get('/cotizacion', function () {
     return view('public.cotizacion');
 });
@@ -25,3 +28,21 @@ Route::post('/admin/personal', [PersonalController::class, 'store'])->name('pers
 Route::get('/admin/personal/{id}/edit', [PersonalController::class, 'edit'])->name('personal.edit');
 Route::put('/admin/personal/{id}', [PersonalController::class, 'update'])->name('personal.update');
 Route::delete('/admin/personal/{id}', [PersonalController::class, 'destroy'])->name('personal.destroy');
+
+//CRUD AMBULANCIAS
+Route::get('/admin/ambulancias', [AmbulanciasController::class, 'index'])->name('ambulancias.index');
+
+
+//CRUD PADECIMIENTOS
+Route::get('/admin/padecimientos', [PadecimientosController::class, 'index'])->name('padecimientos.index');
+
+//CRUD SERVICIOS
+
+
+//CRUD INSUMOS
+Route::get('/admin/insumos', [InsumosController::class, 'index'])->name('insumos.index');
+Route::get('/admin/insumos/create', [InsumosController::class, 'create'])->name('insumos.create');
+Route::post('/admin/insumos', [InsumosController::class, 'store'])->name('insumos.store');
+Route::get('/admin/insumos/{id}/edit', [InsumosController::class, 'edit'])->name('insumos.edit');
+Route::put('/admin/insumos/{id}', [InsumosController::class, 'update'])->name('insumos.update');
+Route::delete('/admin/insumos/{id}', [InsumosController::class, 'destroy'])->name('insumos.destroy');
